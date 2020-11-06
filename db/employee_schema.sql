d14c62c00ebdc505c2e2ec9cd12d96f6f3e7e57a
Sarah Ronald <sareronald@hotmail.com>
2020-11-06 22:06:33 +1100

    setting up tables in mysql

---
+++ db/employee_schema.sql
@@ -0,0 +1,34 @@
+-- Drops the animals_db if it exists currently --
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE DATABASE employee_trackerDB;
+USE employee_trackerDB;
+
+-- Creates the table entitled "department" within employee_trackerDB --
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Creates the table entitled "role" within employee_trackerDB --
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY(id)
+);
+
+-- Creates the table entitled "employee_info" within employee_trackerDB --
+CREATE TABLE employee_info(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES roles(id),
+    FOREIGN KEY(manager_id) REFERENCES employee_info(id)
+);
+
+
