2f4bb97167983912e50e827066f02fa86baaf9f8
Sarah Ronald <sareronald@hotmail.com>
2020-11-06 22:19:47 +1100

    setting up viewAll and viewAllByDept functions

--- employee-tracker.js
+++ employee-tracker.js
@@ -1,10 +1,11 @@
+const logo = require("asciiart-logo");
 const config = require("./package.json");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 const mysql = require("mysql");
 
 // create the connection information for the sql database
-const connection = mysql.createConnection({
+const mysqlConnection = mysql.createConnection({
   host: "localhost",
   port: "root",
   port: 3306,
@@ -14,29 +15,13 @@ const connection = mysql.createConnection({
 });
 
 // connection to the mysql server and sql database
-connection.connect((err) => {
+mysqlConnection.connect((err) => {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
+  console.log("connected as id " + mysqlConnection.threadId + "\n");
   // run the start function after the connection is made to prompt the user
+  title();
   start();
 });
-
-// asciiart-logo displays to start
-console.log(
-  logo({
-    name: "Employee Tracker",
-    font: "ANSI Shadow",
-    lineChars: 5,
-    padding: 2,
-    margin: 3,
-    borderColor: "grey",
-    logoColor: "bold-green",
-    textColor: "green",
-  })
-    .emptyLine()
-    .center("Content Management System")
-    .render()
-);
 
 // function which prompts the user for what action they should take
 function start() {
@@ -60,15 +45,15 @@ function start() {
     ])
     .then(function (answer) {
       switch (answer.action) {
-        case "View all Employees":
+        case "View All Employees":
           viewAllEmployees();
           break;
 
-        case "View all Employees by Department":
+        case "View All Employees by Department":
           viewAllEmployeesByDept();
           break;
 
-        case "View all Employees by Manager":
+        case "View All Employees by Manager":
           viewAllEmployeesByManager();
           break;
 
@@ -89,27 +74,121 @@ function start() {
           break;
 
         case "Quit":
-          connection.end();
+          quit();
           break;
       }
     });
 }
 
 // View All Employees
+// why doesn't SELECT * from employee_info work?
 function viewAllEmployees() {
-  connection.query("SELECT * FROM employee_info", (err, res) => {
+  mysqlConnection.query("SELECT * FROM employee_info", (err, res) => {
     if (err) throw err;
-    console.log(res);
+    console.table(res);
     start();
   });
 }
 
-// View All Employees by Department
-// function viewAllEmployeesByDept() {
-//   connection.query("SELECT ");
-// }
-// View All Employees by Manager
+// // View All Employees by Department
+function viewAllEmployeesByDept() {
+  console.log("Selecting all Employees by Department...\n");
+  mysqlConnection.query(
+    "SELECT department.name FROM department",
+    (err, response) => {
+      if (err) throw err;
+      inquirer
+        .prompt([
+          {
+            name: "deptChoice",
+            type: "rawlist",
+            message: "Which Department would you like to view employees for?",
+            choices() {
+              return response.map((department) => department.name);
+            },
+          },
+        ])
+        .then((answer) => {
+          // get information/employees of the chosen dept
+          // const chosenDept = response.filter((department) => department.name === answer.choices)
+          mysqlConnection.query(
+            `SELECT employee_info.id, employee_info.first_name, employee_info.last_name, roles.title 
+            FROM employee_info, roles, department
+            WHERE employee_info.role_id = roles.id AND roles.department_id = department.id AND department.name = ?`,
+            [answer.deptChoice],
+            (err, res) => {
+              if (err) throw err;
+              console.log(`The employees in the ${answer.deptChoice} are: \n`);
+              console.table(res);
+            }
+          );
+          start();
+        });
+    }
+  );
+}
+
 // Add Employee
-// Remove Employee
-// Update Employee Role
-// Update Employee Manager
+function addEmployee() {
+  // prompt for user input: information on new employee
+  inquirer.prompt([
+    {
+      name: "firstName",
+      type: "input",
+      message: "What is the employee's first name?",
+    },
+    {
+      name: "lastName",
+      type: "input",
+      message: "What is the employee's last name?",
+    },
+    {
+      name: "role",
+      type: "list",
+      message: "What is the employee's role?",
+      // choices: how do I get these from the database?
+      // this will need to be converted to a role id?
+    },
+    {
+      name: "manager",
+      type: "list",
+      message: "Who is the employee's manager?",
+      // choices: how do I get these from the database?
+      // this answer will need to be converted to a manager id?
+    },
+  ]);
+}
+
+// Add departments, roles, employees
+// View departments, roles, employees
+// Update employee roles
+// -- View All Employees by Manager OPTIONAL
+// -- Remove Employee OPTIONAL
+// -- Update Employee Role OPTIONAL
+// -- Update Employee Managers OPTIONAL
+// -- Delete departments, roles and employees
+// -- View total utilized budget
+
+// Quit
+function quit() {
+  mysqlConnection.end();
+}
+
+// asciiart-logo displays to start
+function title() {
+  console.log(
+    logo({
+      name: "Employee Tracker",
+      font: "Doom",
+      lineChars: 5,
+      padding: 2,
+      margin: 3,
+      borderColor: "grey",
+      logoColor: "bold-green",
+      textColor: "green",
+    })
+      .emptyLine()
+      .center("Content Management System")
+      .render()
+  );
+}
